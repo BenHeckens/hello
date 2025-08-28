1bdd49cffc8ebd93d6c4d91dcc7e4186a02e7755
BenHeckens <bheckens2@husker.unl.edu>
2025-08-28 12:45:55 -0500

    inital commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Benjamin Heckens
+ * Date: 2025/08/28
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("BenHeckens\n");
+
+  return 0;
+}
